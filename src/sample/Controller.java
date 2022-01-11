a396b436dd584e05c750992036bf110fe30388cb
migsmo <97015261+migsmo@users.noreply.github.com>
2022-01-11 21:42:07 +0800

    add unpacked bcd conversion

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
 public class Controller {
+    private Converter converter = new Converter();
+
+    public String toUnpackedBCD(int number) {
+        return converter.toUnpackedBCD(number);
+    }
 }
