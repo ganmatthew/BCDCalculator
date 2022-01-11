d46e142d8cca765c83c7df0ed1d4e829ab7ef886
migsmo <97015261+migsmo@users.noreply.github.com>
2022-01-11 22:18:29 +0800

    test for packed BCD

--- src/test/ConverterTest.java
+++ src/test/ConverterTest.java
@@ -5,11 +5,19 @@ import sample.Converter;
 
 public class ConverterTest {
     @Test
-    void test_decimal_to_BCD () {
+    void test_decimal_to_unpackedBCD () {
         Converter converter = new Converter();
 
         String unpackedBCD = converter.toUnpackedBCD(52);
         System.out.println("UnpackedBcd: " + unpackedBCD);
         assert unpackedBCD.equals("0000010100000010");
+    }
+    @Test
+    void test_decimal_to_packedBCD () {
+        Converter converter = new Converter();
+
+        String packedBCD = converter.toPackedBCD(256);
+        System.out.println("packedBCD: " + packedBCD);
+        assert packedBCD.equals("001001010110");
     }
 }
