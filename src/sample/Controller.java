57c4d5a6693e382f3c628ea03a6bf76c56d071e1
Manolo Enriquez <noloenriquez@gmail.com>
2022-02-06 20:04:25 +0800

    added export to text file function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Controller {
     private Converter converter = new Converter();
 
@@ -17,4 +21,36 @@ public class Controller {
         return converter.denselyPackedBCDtoDecimal(dense); 
     }
 
+    public void export(String decimal, String dense, String packed, String unpacked) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            File file = new File("out.txt");
+            FileWriter writer = new FileWriter(file);
+
+            sb.append("Decimal: ");
+            sb.append(decimal);
+            sb.append('\n');
+
+            sb.append("Densely-Packed BCD: ");
+            sb.append(dense);
+            sb.append('\n');
+
+            sb.append("Packed BCD: ");
+            sb.append(packed);
+            sb.append('\n');
+
+            sb.append("Unpacked BCD: ");
+            sb.append(unpacked);
+            sb.append('\n');
+
+            writer.write(sb.toString());
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
